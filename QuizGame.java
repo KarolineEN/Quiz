119a079d08752ff3b136cfbd424b0b6810830361
Anders Borger Olsen <anders.borger.olsen@hotmail.no>
2024-03-07 18:03:35 +0100

    2

--- QuizGame.java
+++ QuizGame.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class QuizGame{
 
     //#Fields
-    String question1 ="Hvilket sjokolade pålegg inneholder mest sjokolade?";
+    String question1 = "Hvilket sjokolade pålegg inneholder mest sjokolade?";
     String[] answers1 = {
             " ",
             "Nugatti",
             "Nutella",
             "Sjokade"};
-    ----------------
-    String question2 = "Hvilken planet i sol systemet er kjent for sine karakteristiske ringer?"
-            + " " + "Mars, Jupiter, Saturn eller Venus";
-    String answer2 = "Saturn";
-    ---------------
-    String question3="Hva er ulovlig å gjøre på et hotellrom i California?"
-            +" Å skrelle en løk, Å be til gud, Å høre på musikk etter 18.00,Å danse?";
-    String answer3="Å skrelle en løk";
-    --------------
+    //----------------
+    String question2 = "Hvilken planet i sol systemet er kjent for sine karakteristiske ringer?";
+    String[] answers2 = {
+            " ",
+            "Mars",
+            "Jupiter",
+            "Saturn",
+            "Venus"};
+    //---------------
+    String question3 = "Hva er ulovlig å gjøre på et hotellrom i California?";
+
+    String[] answers3 = {
+            "Å skrelle en løk",
+            "Å be til gud",
+            "Å høre på musikk etter 18.00",
+            "Å danse"
+    };
+    //--------------
     JavaProgram newJavaProgram = new JavaProgram();
 
     Scanner quizAnswer = new Scanner(System.in);
@@ -47,8 +56,11 @@ public class QuizGame{
 
     public void question2(){
         System.out.println(question2);
+        for (int i = 0; i < answers2.length; i++){
+            System.out.println(answers2[i]);
+        }
         String userAnswer = quizAnswer.nextLine();
-        if(userAnswer.equals(answer2)){
+        if(userAnswer.equals(answers2[3])){
             System.out.println("Riktig!");
             userPoints += 10;
             System.out.println("Du har totalt" + " " + userPoints + "poeng");
@@ -59,8 +71,12 @@ public class QuizGame{
     }
     public void question3(){
         System.out.println(question3);
-        String userAnswer=quizAnswer.nextLine();
-        if(userAnswer.equals(answer3)){
+        for (int i = 0;i < answers3.length; i++){
+            System.out.println(answers3[i]);
+        }
+
+        String userAnswer = quizAnswer.nextLine();
+        if(userAnswer.equals(answers3[0])){
             System.out.println("Riktig!");
             userPoints += 10;
             System.out.println("Du har totalt" + " " + userPoints + "poeng");
